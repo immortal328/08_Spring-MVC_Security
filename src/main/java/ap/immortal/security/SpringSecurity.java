7051c620fd9f470e55229374af085a7404635ddc
immortal328 <amar.pujari28@gmail.com>
2024-06-08 22:45:40 +0530

    Added Custom login Page

--- src/main/java/ap/immortal/security/SpringSecurity.java
+++ src/main/java/ap/immortal/security/SpringSecurity.java
@@ -2,9 +2,11 @@ package ap.immortal.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration 
 public class SpringSecurity {
@@ -15,6 +17,21 @@ public class SpringSecurity {
 		UserDetails amar = User.builder().username("amar").password("{noop}amar").roles("ADMIN").build();
 		UserDetails avi = User.builder().username("avi").password("{noop}avi").roles("MANAGER").build();
 		return new InMemoryUserDetailsManager(amar,amit,avi);
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
+		http.authorizeHttpRequests(
+				configure -> configure
+							.anyRequest()
+							.authenticated()
+				).formLogin(
+						form -> form
+								.loginPage("/showMyLoginForm")
+								.loginProcessingUrl("/authenticateTheUser")
+								.permitAll()
+				);
+		return http.build();
 	}
 	
 	
