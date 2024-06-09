9673970b30feb5a3d4c701909c9f44aec75c4fc7
immortal328 <amar.pujari28@gmail.com>
2024-06-09 18:26:00 +0530

    Added JDBC database Authentication - plain text

---
+++ src/main/java/ap/immortal/security/SpringSecurityInMemoryUse.java
@@ -0,0 +1,47 @@
+package ap.immortal.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration 
+public class SpringSecurityInMemoryUse {
+	
+	@Bean
+	InMemoryUserDetailsManager userDetailsManger() {
+		UserDetails amit = User.builder().username("amit").password("{noop}amit").roles("EMPLOYEE").build();
+		UserDetails amar = User.builder().username("amar").password("{noop}amar").roles("ADMIN").build();
+		UserDetails avi = User.builder().username("avi").password("{noop}avi").roles("MANAGER").build();
+		return new InMemoryUserDetailsManager(amar,amit,avi);
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
+		http.authorizeHttpRequests(
+				configure -> configure
+			                .requestMatchers("/").hasAnyRole("EMPLOYEE","MANAGER","ADMIN")
+			                .requestMatchers("/leaders/**").hasRole("MANAGER")
+			                .requestMatchers("/systems/**").hasRole("ADMIN")
+			                .anyRequest().authenticated()
+				).formLogin(
+						form -> form
+								.loginPage("/showMyLoginForm")
+								.loginProcessingUrl("/authenticateTheUser")
+								.permitAll()
+				).logout(
+						logout -> logout
+								  .permitAll()
+				).exceptionHandling(
+						configure -> configure
+						 .accessDeniedPage("/access-denied"));
+		
+		return http.build();
+	}
+	
+	
+
+}
--- src/main/java/ap/immortal/security/SpringSecurityJDBC.java
+++ src/main/java/ap/immortal/security/SpringSecurityJDBC.java
@@ -1,22 +1,21 @@
 package ap.immortal.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration 
-public class SpringSecurity {
+@Configuration
+public class SpringSecurityJDBC {
 	
+	//Use JDBC Database 
 	@Bean
-	InMemoryUserDetailsManager userDetailsManger() {
-		UserDetails amit = User.builder().username("amit").password("{noop}amit").roles("EMPLOYEE").build();
-		UserDetails amar = User.builder().username("amar").password("{noop}amar").roles("ADMIN").build();
-		UserDetails avi = User.builder().username("avi").password("{noop}avi").roles("MANAGER").build();
-		return new InMemoryUserDetailsManager(amar,amit,avi);
+	UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 	
 	@Bean
@@ -43,5 +42,7 @@ public class SpringSecurity {
 	}
 	
 	
+
+
 
 }
